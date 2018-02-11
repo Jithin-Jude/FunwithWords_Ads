5e39581b37ccf6005563c662ba058abb2054157f
Jithin K Jose <jithin.jude68@gmail.com>
2018-02-12 00:42:37 +0530

    version 2

---
+++ app/src/main/java/com/inc/mountzoft/funwithwords/Tab2.java
@@ -0,0 +1,22 @@
+package com.inc.mountzoft.funwithwords;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.inc.mountzoft.funwithwords.R;
+
+/**
+ * Created by JithinJude on 30-01-2018.
+ */
+
+public class Tab2 extends android.support.v4.app.Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2_layout, container, false);
+        return rootView;
+    }
+}
